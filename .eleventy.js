8463623dbdbe433960a8c8ae569775f0040440a2
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-15 15:23:55 +0200

    fix: Correct Eleventy config to accept yaml files

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addDataExtension("yaml,yml", (contents) => yaml.load(contents));
 
   return {
     dir: {
